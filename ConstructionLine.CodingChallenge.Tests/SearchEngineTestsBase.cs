796124801b4906caf66b5722cbe0c1c2294e919e
Simon Chamberlain <simonalexchamberlain@googlemail.com>
2020-06-16 09:29:22 -0700

    -Upgraded .net core to 3.1 (test runner issues) -implemented search engine functionality -fixed test base class bug

--- ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
+++ ConstructionLine.CodingChallenge.Tests/SearchEngineTestsBase.cs
@@ -39,7 +39,10 @@ namespace ConstructionLine.CodingChallenge.Tests
 
                 var expectedSizeCount = shirts
                     .Count(s => s.Size.Id == size.Id
-                                && (!searchOptions.Colors.Any() || searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id)));
+                                && (!searchOptions.Colors.Any() ||
+                                    searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id))
+                                && (!searchOptions.Sizes.Any() ||
+                                    searchOptions.Sizes.Select(sz => sz.Id).Contains(s.Size.Id)));
 
                 Assert.That(sizeCount.Count, Is.EqualTo(expectedSizeCount), 
                     $"Size count for '{sizeCount.Size.Name}' showing '{sizeCount.Count}' should be '{expectedSizeCount}'");
@@ -57,8 +60,11 @@ namespace ConstructionLine.CodingChallenge.Tests
                 Assert.That(colorCount, Is.Not.Null, $"Color count for '{color.Name}' not found in results");
 
                 var expectedColorCount = shirts
-                    .Count(c => c.Color.Id == color.Id  
-                                && (!searchOptions.Sizes.Any() || searchOptions.Sizes.Select(s => s.Id).Contains(c.Size.Id)));
+                    .Count(s => s.Color.Id == color.Id
+                                && (!searchOptions.Colors.Any() ||
+                                    searchOptions.Colors.Select(c => c.Id).Contains(s.Color.Id))
+                                && (!searchOptions.Sizes.Any() ||
+                                    searchOptions.Sizes.Select(sz => sz.Id).Contains(s.Size.Id)));
 
                 Assert.That(colorCount.Count, Is.EqualTo(expectedColorCount),
                     $"Color count for '{colorCount.Color.Name}' showing '{colorCount.Count}' should be '{expectedColorCount}'");
--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConstructionLine.CodingChallenge
 {
@@ -17,10 +20,40 @@ namespace ConstructionLine.CodingChallenge
 
         public SearchResults Search(SearchOptions options)
         {
-            // TODO: search logic goes here.
+            if (options == null) throw new ArgumentNullException(nameof(options));
+            if (options.Colors == null && options.Sizes == null) throw new ArgumentNullException(nameof(options));
+
+            var results = _shirts.Where(s => (!options.Colors.Any() || options.Colors.Any(x => x == s.Color)
+                && (!options.Sizes.Any() || options.Sizes.Any(x => x == s.Size)))).ToList();
+
+
+
+
+            var byColour = results.GroupBy(x => x.Color)
+                .Select(x => new ColorCount
+                {
+                    Color = x.Key,
+                    Count = x.Count()
+                }).ToList();
+
+            var bySize = results.GroupBy(x => x.Size)
+                .Select(x => new SizeCount
+                {
+                    Size = x.Key,
+                    Count = x.Count()
+                }).ToList();
+
+            var missingColours = Color.All.Where(c => byColour.All(x => x.Color != c))
+                .Select(x => new ColorCount {Color = x, Count = 0});
+
+            var missingSizes = Size.All.Where(c => bySize.All(x => x.Size != c))
+                .Select(x => new SizeCount { Size = x, Count = 0 });
 
             return new SearchResults
             {
+                ColorCounts = byColour.Union(missingColours).ToList(),
+                Shirts = results,
+                SizeCounts = bySize.Union(missingSizes).ToList()
             };
         }
     }
